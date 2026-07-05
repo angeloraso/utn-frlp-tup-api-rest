b72bb190b9a912f857e7ac3b518485ee7d420394
Angelo Raso <araso@anura.com.ar>
2026-07-05 20:28:47 -0300

    Update error message

--- server.js
+++ server.js
@@ -177,7 +177,7 @@ async function authenticate(req, res, next) {
         next();
     } catch (error) {
         return res.status(401).json({
-            message: "Invalid token",
+            message: error.message,
         });
     }
 }
